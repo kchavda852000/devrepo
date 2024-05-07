39f9a4037263847351dc685a9c1fcac6674ef927
Ketul Chavda <Ketul.Chavda@ats-global.com>
2024-05-07 22:01:55 +0530

    Update2

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-//updatedgit1
+//updatedgit2
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
