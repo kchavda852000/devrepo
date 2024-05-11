eece1171a4cffd76682243113897edbe613ee10e
Ketul Chavda <Ketul.Chavda@ats-global.com>
2024-05-11 15:53:41 +0530

    testapiupadate3

--- src/pages/api/testapi.js
+++ src/pages/api/testapi.js
@@ -1,7 +1,5 @@
-import servershutdown from "@/lib/servershutdown";
-
 export default async function handler(req, res) {
   //   console.log("in API Internal", req.body);
-  let dbData = await servershutdown();
-  res.send(JSON.stringify({ test: "updated2" }));
+  //   let dbData = await servershutdown();
+  res.send(JSON.stringify({ test: "updated3" }));
 }
