57430f6916658113551c06cb8294c57d1534134d
Ketul Chavda <Ketul.Chavda@ats-global.com>
2024-05-11 15:22:32 +0530

    creatednewtestapi

---
+++ src/pages/api/testapi.js
@@ -0,0 +1,7 @@
+import servershutdown from "@/lib/servershutdown";
+
+export default async function handler(req, res) {
+  //   console.log("in API Internal", req.body);
+  let dbData = await servershutdown();
+  res.send(JSON.stringify({ test: "updated1" }));
+}
